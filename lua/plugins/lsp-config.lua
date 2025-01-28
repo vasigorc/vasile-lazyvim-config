a8889b18615e33b49958190d8768ecb129a629ab
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-01-27 19:33:32 -0500

    chown: Added typescript config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,49 @@
+return {
+  -- Configure additional TypeScript/JavaScript specific settings
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Add any additional tsserver settings here
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- Ensure the necessary tools are installed through Mason
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "eslint-lsp", -- ESLint LSP
+        "prettier", -- Prettier formatter
+      })
+    end,
+  },
+}
