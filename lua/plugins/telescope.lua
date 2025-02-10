41e47ed94683e14afc6ec9c3f4584035dfdb7e02
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-02-09 23:06:00 -0500

    chore: Updated plugins

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        find_files = {
+          attach_mappings = function(_, map)
+            map("i", "<CR>", function(bufnr)
+              -- This makes telescope open the file in the current window only
+              require("telescope.actions.set").edit(bufnr, "edit")
+            end)
+            return true
+          end,
+        },
+      },
+    },
+  },
+}
