da5b60a84d3c5dc94041eefdb37d8f8ab689b720
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-05-12 15:41:04 -0400

    chore: Added chat history extension to CodeCompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/codecompanion-history.nvim",
   },
   config = function()
     require("codecompanion").setup({
@@ -29,6 +30,29 @@ return {
           opts = {
             show_default_actions = true,
             show_default_prompt_library = true,
+          },
+        },
+      },
+      extensions = {
+        -- gh - Open history browser
+        -- sc - Save current chat manually
+        -- <C-R> - Open selected chat
+        -- d - Delete selected chat in normal mode
+        history = {
+          enabled = true,
+          opts = {
+            -- Keymap to open history from chat buffer (default: gh)
+            keymap = "gh",
+            auto_generate_title = true,
+            continue_last_chat = false,
+            -- When chat is cleared with `gx` delete from chat history
+            delete_on_clearning_chat = false,
+            picker = "telescope",
+            enable_logging = false,
+            ---Directory path to save the chats
+            dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+            auto_save = true,
+            save_chat_keymap = "sc",
           },
         },
       },
