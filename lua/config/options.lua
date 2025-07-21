a63a17ab40c27114147fd50cca44a2490fc8715c
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-07-20 21:24:19 -0400

    fix: Fixed a typo in rust analyzer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,5 +19,5 @@ vim.opt.relativenumber = false -- Disable relative line numbers
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
 -- LSP server to use for Rust
-vim.g.lazyvim_rust_diagnostics = "rust_analyzer"
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
