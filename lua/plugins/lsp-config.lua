1837c40190acbf7b1f2d48a8a488788b2a302155
Vasile Gorcinschi <vgorcinschi@ncl.com>
2025-10-16 06:27:05 -0400

    fix: Fixed JS/TS support
    Removed conflicting configs for ts_server and vstls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,35 +7,9 @@ return {
         protols = {
           filetypes = { "proto" },
         },
-        -- Add any additional tsserver settings here
-        tsserver = {
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
-        },
+        -- Clangd configuration
         clangd = {
-          filetypes = { "c", "cpp", "objc", "objcpp" }, -- explicitly omitting .proto
+          filetypes = { "c", "cpp", "objc", "objcpp" },
         },
       },
     },
@@ -47,10 +21,10 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        "eslint-lsp", -- ESLint LSP
-        "prettier", -- Prettier formatter
-        "protols", -- Protobuf support
-        "sql-formatter", -- https://github.com/sql-formatter-org/sql-formatter
+        "eslint-lsp",
+        "prettier",
+        "protols",
+        "sql-formatter",
       })
     end,
   },
