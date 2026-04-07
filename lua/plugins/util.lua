e0f2d57ac5c39e19c25c64ecbdcacd08bb7088ba
Vasile Gorcinschi <vasile.gorcinschi@shopify.com>
2026-04-07 13:05:10 -0400

    feat: add GitHub emoji shortcodes to Snacks icon picker

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -4,7 +4,16 @@ return {
     "snacks.nvim",
     opts = {
       bigfile = { enabled = false },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          icons = {
+            custom_sources = {
+              github_shortcodes = vim.fn.stdpath("config") .. "/data/github-emoji-shortcodes.json",
+            },
+          },
+        },
+      },
     },
   },
 }
