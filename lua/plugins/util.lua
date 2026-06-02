120520e8f55694349e123193892e8c5f053fb199
Vasile Gorcinschi <vasile.gorcinschi@shopify.com>
2026-06-02 11:43:55 -0400

    fix: map gitstream remote for Snacks gitbrowse

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -4,6 +4,16 @@ return {
     "snacks.nvim",
     opts = {
       bigfile = { enabled = false },
+      gitbrowse = {
+        config = function(opts)
+          -- Keep the git remote as gitstream, but browse Shopify's monorepo on GitHub.
+          opts.remote_patterns = opts.remote_patterns or {}
+          table.insert(opts.remote_patterns, 1, {
+            "^https://gitstream%.shopify%.io/shop/world%.git$",
+            "https://github.com/shop/world.git",
+          })
+        end,
+      },
       picker = {
         enabled = true,
         sources = {
